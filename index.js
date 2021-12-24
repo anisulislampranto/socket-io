9b19db47d1e76ad0d14ad71bfad20243d96c0e38
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2021-12-24 18:00:03 +0600

    discovered connection disconnection And WS Protocol

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+require('dotenv').config()
 
 const port = process.env.PORT || 4040
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
